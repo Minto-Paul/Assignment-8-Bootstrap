ff52dbad14402da6f32c1140cee4d0f4262116f3
Minto-Paul <56397412+Minto-Paul@users.noreply.github.com>
2019-11-28 21:09:48 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+function animateCSS(element, animationName, callback) {
+    var node = document.querySelector(element)
+    node.classList.add('animated', animationName)
+
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName)
+        node.removeEventListener('animationend', handleAnimationEnd)
+
+        if (typeof callback === 'function') callback()
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
+}
+
+
+animateCSS('.navbar-brand', 'bounceInLeft');
